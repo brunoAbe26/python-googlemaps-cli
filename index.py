e44fa1f0ffe2b3ef055c297a5518c41639a007eb
brunoAbe26 <darksorcerer43@hotmail.com>
2019-04-27 20:07:26 -0300

    Connectando o database

--- index.py
+++ index.py
@@ -3,30 +3,40 @@ import sys
 import googlemaps
 import urllib
 import MySQLdb
+from datetime import datetime
 
-# URL = 'https://api.darksky.net/forecast/1f6bf3c3f3b572567ae46ac0343b974c/'
-# KEY = 'AIzaSyBtFVBD73TGr0-jAGUq_3qO-o1StlH2PsY'
+URL = 'https://api.darksky.net/forecast/1f6bf3c3f3b572567ae46ac0343b974c/'
+KEY = 'AIzaSyBtFVBD73TGr0-jAGUq_3qO-o1StlH2PsY'
 
-# local = ' '.join(sys.argv[1:])
+local = ' '.join(sys.argv[1:])
 
-# gmaps = googlemaps.Client(key=KEY)
-# geocode_result = gmaps.geocode(local)
-# location = geocode_result[0]['geometry']['location']
+gmaps = googlemaps.Client(key=KEY)
+geocode_result = gmaps.geocode(local)
+location = geocode_result[0]['geometry']['location']
 
-# PARAMS = {
-#     'units': 'si',
-#     'exclude': 'minutely,hourly,daily,alerts,flags'
-# }
+PARAMS = {
+    'units': 'si',
+    'exclude': 'minutely,hourly,daily,alerts,flags'
+}
 
-# # request = requests.get(url=f"{URL}{location['lat']},{location['lng']}/?units=si&exclude=minutely,hourly,daily,alerts,flags")
-# request = requests.get(url=f"{URL}{location.get('lat')},{location.get('lng')}", params=PARAMS)
-# print('type before', request)
-# response = request.json()
-# print('type', type(response))
-# print(response)
+# request = requests.get(url=f"{URL}{location['lat']},{location['lng']}/?units=si&exclude=minutely,hourly,daily,alerts,flags")
+request = requests.get(url=f"{URL}{location.get('lat')},{location.get('lng')}", params=PARAMS)
+print('type before', request)
+response = request.json()
+print('type', type(response))
+print(response)
 
-# print(f"Temperatura: {response['currently']['temperature']} ºC")
-# print(f"Sensação térmica: {response['currently']['apparentTemperature']} ºC")
+temp = response['currently']['temperature']
+sencacao = response['currently']['apparentTemperature']
+print(f"Temperatura: {temp} ºC")
+print(f"Sensação térmica: {sencacao} ºC")
 
-conn = MySQLdb.connect(db='world', user='brunoabe', password='91946624')
+conn = MySQLdb.connect(db='temperatura', user='root', password='rocambole')
+cur = conn.cursor()
+cur.execute(f'''
+        INSERT INTO consultas (local, temp, sensacao_termica, data_consulta) VALUES('{local}', {temp}, {sencacao}, '{datetime.now()}')
+    ''')
+conn.commit()
+cur.close()
+
 
